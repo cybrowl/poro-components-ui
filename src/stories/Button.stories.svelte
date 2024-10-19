48e4db552cdc392d145e8d2c652254b1baf76145
cyberowl <cybrowl@protonmail.com>
2024-10-19 00:56:00 -0700

    add: support for addon-svelte-csf

---
+++ src/stories/Button.stories.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	import Button from '../lib/Button.svelte';
+
+	export const meta = {
+		title: 'Button',
+		component: Button
+	};
+</script>
+
+<script>
+	import {Template, Story} from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story name="Blue" args={{label: 'button'}} />
+<Story name="Square" args={{label: 'square button'}} />
