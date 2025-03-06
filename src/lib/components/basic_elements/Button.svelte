f29ed145f179bcc8f52231774b102cdea540fc8b
cyberowl <cybrowl@protonmail.com>
2025-03-05 23:30:51 -0800

    refactor: code for FundWalletWarning and Button

--- src/lib/components/basic_elements/Button.svelte
+++ src/lib/components/basic_elements/Button.svelte
@@ -1,14 +1,23 @@
 <script>
-	const {label = 'Button', variant = 'dark', width = 'w-auto'} = $props();
+	let {
+		label = 'Button',
+		variant = 'dark',
+		width = 'w-auto',
+		height = 'h-14'
+	} = $props();
+
+	let buttonClasses = $derived(
+		`base-button ${variant === 'dark' ? 'dark' : 'gold'} ${width} ${height}`
+	);
 </script>
 
-<button class={`base-button ${variant === 'dark' ? 'dark' : 'gold'} ${width}`}>
+<button class={buttonClasses}>
 	{label}
 </button>
 
 <style lang="postcss">
 	.base-button {
-		@apply inline-flex items-center justify-center px-4 py-2 h-14 rounded-md font-medium;
+		@apply inline-flex items-center justify-center px-4 py-2 rounded-md font-medium;
 	}
 
 	.dark {
--- src/lib/components/wallet/FundWalletWarning.svelte
+++ src/lib/components/wallet/FundWalletWarning.svelte
@@ -3,6 +3,17 @@
 	import Button from '../basic_elements/Button.svelte';
 </script>
 
+{#snippet WarningMessage()}
+	<p class="message">Please ensure that your wallet has sufficient funds!</p>
+{/snippet}
+
+{#snippet ActionButtons()}
+	<div class="actions">
+		<Button label="Ok" variant="dark" width="w-16" height="h-14" />
+		<Button label="Fund wallet" variant="gold" width="w-32" height="h-14" />
+	</div>
+{/snippet}
+
 <div class="wallet-funds-warning">
 	<Icon
 		name="piggy_bank_placeholder"
@@ -11,24 +22,19 @@
 		scale="1"
 		viewSize={{width: 64, height: 64}}
 	/>
-
-	<p class="message">Please ensure that your wallet has sufficient funds!</p>
-
-	<div class="actions">
-		<Button label="Ok" variant="dark" width="w-16" />
-		<Button label="Fund wallet" variant="gold" width="w-32" />
-	</div>
+	{@render WarningMessage()}
+	{@render ActionButtons()}
 </div>
 
 <style lang="postcss">
 	.wallet-funds-warning {
 		@apply bg-background-primary
-        font-sans
-        flex flex-col items-center justify-center
-        w-[672px] h-[400px] p-6
-        text-center
-        rounded-xl
-        border border-white border-opacity-25;
+      font-sans
+      flex flex-col items-center justify-center
+      w-[672px] h-[400px] p-6
+      text-center
+      rounded-xl
+      border border-white border-opacity-25;
 	}
 
 	.message {
